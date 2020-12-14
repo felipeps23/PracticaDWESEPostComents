08d90c5b57a26f6ae4b3ab1ee93eca12a1769fe7
felipeps23 <48446348+felipeps23@users.noreply.github.com>
2020-12-14 11:51:22 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,12 @@ Route::resource('backend/post', BackendPostController::class,['names' => 'backen
 
 Route::resource('backend/coment', BackendComentController::class,['names' => 'backend.coment']);
 
-Route::get('backend/coment/create/{idcoment}', [BackendComentController::class, 'createComentPo'])->name('backend.ticket.createcomentpo');
+Route::get('backend/coment/create/{idcoment}', [BackendComentController::class, 'createComentPo'])->name('backend.createcomentpo');
 
-Route::get('backend/coment/{idpost}/coment', [BackendComentController::class, 'showComents'])->name('backend.ticket.showcoments');
+Route::get('backend/coment/{idpost}/coment', [BackendComentController::class, 'showComents'])->name('backend.showcoments');
 
 Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
 Route::get('/frontend', [FrontendController::class, 'index'])->name('frontend.index');
 
 Route::get('/frontend/{idpost}', [FrontendController::class, 'show'])->name('frontend.show');
+Route::post('frontend', [FrontendController::class, 'store'])->name('frontend.store');
